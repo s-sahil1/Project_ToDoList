4bb6240c884614d46e6ce6b623eff954bec10807
s-sahil1 <35522593+s-sahil1@users.noreply.github.com>
2022-08-30 00:18:11 -0300

    upload 1

---
+++ index.js
@@ -0,0 +1,64 @@
+var form;
+
+
+$("button").click(function() {
+  form = $(".input-field").val();
+  if (form != "") {
+    console.log(form);
+    //spin();
+    rotate(0);
+
+    $("ul").append("<li>" + form + "</li>");
+  }
+
+  $(".input-field").val("");
+
+
+});
+
+$(".reset").click(function(){
+  $("ul").remove();
+});
+
+// function spin() {
+//
+//   $("button").addClass("rotate1");
+//
+//   setTimeout(function() {
+//     $("button").removeClass("rotate1");
+//   }, 300);
+//
+// $(document).ready(function() {
+//
+//   setTimeout(function() {
+//     $("button").addClass("rotate2");
+//   }, 300);
+// });
+//
+// $(document).ready(function() {
+//   setTimeout(function() {
+//     $("button").removeClass("rotate2");
+//   }, 300);
+//
+// });
+// }
+
+function rotate(degree) {
+
+  // For webkit browsers: e.g. Chrome
+  $("button").css({
+    WebkitTransform: 'rotate(' + degree + 'deg)'
+  });
+  // For Mozilla browser: e.g. Firefox
+  $("button").css({
+    '-moz-transform': 'rotate(' + degree + 'deg)'
+  });
+
+  degree=degree+5;
+
+  // Animate rotation with a recursive call
+  setTimeout(function() {
+    if(degree<=360){
+    rotate(degree);}
+  }, 0.5);
+}
